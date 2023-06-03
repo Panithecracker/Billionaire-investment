173f48973f7a5498c14ade5cd43acad9eb5b5a86
Panithecracker <97905110+Panithecracker@users.noreply.github.com>
2023-06-03 14:54:21 +0200

    Update main.cpp
    Now it works for any min capital desired. Only thing left is to conclude a better guess value to be used for this general case in the Newton root finding method

--- main.cpp
+++ main.cpp
@@ -6,12 +6,20 @@ using namespace std;
 
 int main()
 {
-    double optimal_f = NewtonRaphson(0.2,25,0.001); 
-    int min_heads = MinHeads(optimal_f);
+
+    // Step 1 : Compute the value of f that minimizes the amount of heads needed to make at least a billion dollars
+    //minimum capital to make is c:
+    //for the specific problem it is a billion, however it works with any other value
+    double c = pow(10.0,9.0);
+    double optimal_f = NewtonRaphson(0.2,25,0.001,c ); 
+    int min_heads = MinHeads(optimal_f, c);
     cout << "Optimal proportion is estimated to be -> " << optimal_f << endl;
-    cout << "In fact the min heads in the game to ensure billion gains is -> " << MinHeads(optimal_f) << endl; 
+    cout << "In fact the min heads in the game to ensure billion gains is -> " << min_heads << endl; 
+    
     return 0;
 }
+
+
 
 
 
